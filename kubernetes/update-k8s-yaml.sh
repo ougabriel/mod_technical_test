d7ab0d8edbf9f33d23e9dfcf374a8781b4fa5f30
Gabriel-Okom <ougabriel@gmail.com>
2024-10-20 16:11:17 +0100

    Bash script to update using ecr url and tag

--- kubernetes/update-k8s-yaml.sh
+++ kubernetes/update-k8s-yaml.sh
@@ -2,22 +2,18 @@
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
-# Arguments
-AWS_ACCOUNT_ID=$1
-ECR_REPOSITORY=$2
-TAG=${3:-latest}  # Default to "latest" if not provided
-DEPLOYMENT_FILE=$4
+# Constants
+DEPLOYMENT_FILE="./kubernetes/deployment.yaml"
+ECR_IMAGE="019485243761.dkr.ecr.eu-west-2.amazonaws.com/gabapprepodev:latest"
 
-# Fetch the latest image tag from ECR
-LATEST_IMAGE=$(aws ecr describe-images --repository-name "$ECR_REPOSITORY" --query 'imageDetails[?contains(imageTags, `'"$TAG"'`)].imageTags[0]' --output text --region eu-west-2)
-
-# Check if the LATEST_IMAGE is valid
-if [ -z "$LATEST_IMAGE" ] || [ "$LATEST_IMAGE" == "None" ]; then
-    echo "Error: Unable to find the latest image tag in the repository."
+# Check if the deployment file exists
+if [[ ! -f "$DEPLOYMENT_FILE" ]]; then
+    echo "Error: Deployment file $DEPLOYMENT_FILE does not exist."
     exit 1
 fi
 
-# Replace the image in the deployment YAML file
-sed -i "s|image: \".*\"|image: \"$AWS_ACCOUNT_ID.dkr.ecr.eu-west-2.amazonaws.com/$ECR_REPOSITORY:$LATEST_IMAGE\"|" "$DEPLOYMENT_FILE"
+# Update the image in the deployment YAML file
+sed -i.bak "s|image: \".*\"|image: \"$ECR_IMAGE\"|" "$DEPLOYMENT_FILE"
 
-echo "Updated $DEPLOYMENT_FILE with image: $AWS_ACCOUNT_ID.dkr.ecr.eu-west-2.amazonaws.com/$ECR_REPOSITORY:$LATEST_IMAGE"
+# Inform the user of the update
+echo "Updated $DEPLOYMENT_FILE with image: $ECR_IMAGE"
